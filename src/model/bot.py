14422a35c5ba5f98cb41c0a95b23cd548f83a552
Paulo Welton <paulinho.welton08@gmail.com>
2025-04-08 09:52:20 -0300

    correcao em probemas na funcao de callback_query() e criacao de dict pra armazenar dados dos usuarios

--- src/model/bot.py
+++ src/model/bot.py
@@ -17,20 +17,68 @@ def sanitize_filename(filename):
     return re.sub(r'[^a-zA-Z0-9_-]', '_', filename)
 
 def bot():
-    links = []
-    
-    filenames = []
-    
     print('bot chamado')
     log.info('bot chamado')
     
     bot = telebot.TeleBot(BOT_TOKEN)
-    
     bot.remove_webhook()
+    
+    user_data = {}
     
     @bot.message_handler(commands=['start', 'hello'])
     def send_instructions(message):
         bot.reply_to(message, "Olá eu sou um Bot de Download\n\nMe envie um link do Youtube")
+        
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_query(call):
+        chat_id = call.message.chat.id
+        message_id = call.message.message_id
+        
+        try:
+            if chat_id not in user_data:
+                bot.answer_callback_query(call.id, 'Sessão expirada!')
+                return
+            
+            data = user_data[chat_id]
+            link = data['link']
+            titulo = data['title']
+            filename = data['filename']
+            
+            if call.data == "option_audio":
+                print('usuario pediu a opcao audio')
+                bot.send_message(chat_id, 'Aguardando Download...')
+                
+                caminho = download_audio(link, filename)
+                
+                with open(caminho, "rb") as audio:
+                    bot.send_audio(chat_id, audio, caption=f"<b>{titulo}</b>\n\n<b>Criador: </b><a href='https://github.com/paulowelton'>GitHub</a>", parse_mode='HTML')
+                apagar_arquivos(caminho)
+                
+                print('audio enviado\n')
+                                                                                                                                         
+            if call.data == "option_video":
+                pass
+            
+            if call.data == "option_cancel":
+                log.info('usuario cancelou o download')
+                
+                bot.delete_message(chat_id, message_id)
+                bot.send_message(chat_id, 'Canceled')
+            
+            # deleta as mesagens
+            try:
+                bot.delete_message(chat_id, message_id)
+            except:
+                print('mensagem não deletada')
+                log.info('mensagem não deletada')
+            
+            # limpa os dados do usuarios
+            user_data.pop(chat_id, None)    
+        except Exception as e:
+            log.info(e)
+            print(e)
+            
+            bot.send_message(call.message.chat.id, "Ocorreu um erro ao processar sua escolha.")
     
     @bot.message_handler(func=lambda message: re.search(YOUTUBE_REGEX, message.text))
     def handle_youtube_link(message):
@@ -38,88 +86,29 @@ def bot():
             log.info('usuario esta tentando fazer download')
             print('usuario esta tentando fazer um download')
             
+            chat_id = message.chat.id
+            
             youtube_link = message.text.strip()
-            
-            links.append(youtube_link)
-            
-            markup = InlineKeyboardMarkup()
-            option_video = InlineKeyboardButton("Video", callback_data="option_video")
-            option_audio = InlineKeyboardButton("Audio", callback_data="option_audio")
-            option_cancel = InlineKeyboardButton("Cancel", callback_data="option_cancel")
-            markup.add(option_video)
-            markup.add(option_audio)
-            markup.add(option_cancel)
-            
-            titulo_antigo = get_title(youtube_link)
-            titulo = sanitize_filename(titulo_antigo)   
-            
+            titulo = sanitize_filename(get_title(youtube_link))
             thumb = get_thumb(youtube_link)
+            
+            user_data[chat_id] = {
+                'link': youtube_link,
+                'title': titulo,
+                'filename': f'{titulo}.m4a'
+            }
             
             print(f'{titulo}: {youtube_link}')
             
-            filenames.append(titulo + '.m4a')
+            markup = InlineKeyboardMarkup()
+            markup.add(
+            InlineKeyboardButton("Video", callback_data="option_video"),
+            InlineKeyboardButton("Audio", callback_data="option_audio"),
+            InlineKeyboardButton("Cancel", callback_data="option_cancel")
+            )
             
-            message = bot.send_photo(message.chat.id, thumb, f'<b>{titulo}</b>\n\n<b>Escolha uma opção</b>', reply_markup=markup, parse_mode='HTML')
+            message = bot.send_photo(chat_id, thumb, f'<b>{titulo}</b>\n\n<b>Escolha uma opção</b>', reply_markup=markup, parse_mode='HTML')
             
-            @bot.callback_query_handler(func=lambda call: True)
-            def callback_query(call):
-                if call.data == "option_video":
-                    bot.send_message(message.chat.id, 'Aguardando download...')
-                    
-                    login = os.getlogin()
-                    caminho_video = f'C:\\Users\\{login}\\Desktop\\bot_telegram_youtube\\video\\{titulo}.mp4'
-                    
-                    download_video(youtube_link, titulo)
-                    
-                    if os.path.getsize(caminho_video) > 50 * 1024 * 1024:
-                        bot.send_message(message.chat.id, 'Erro, este video tem um limite de 50mb')
-                        
-                        #bot.delete_message(message.chat.id, message.message_id)
-                    
-                        apagar_arquivos(caminho_video)
-                    else:
-                        try:
-                            with open(caminho_video, 'rb') as video:
-                                bot.send_video(message.chat.id, video)
-                                
-                            bot.delete_message(message.chat.id, message.message_id)
-                        
-                            apagar_arquivos(caminho_video)
-                        except:
-                            bot.send_message(message.chat.id, 'Erro, tente Novamente')
-
-
-
-
-
-
-
-
-                if call.data == "option_audio":
-                    print('usuario pediu a opcao audio')
-                    bot.send_message(message.chat.id, 'Aguardando Download...')
-                    
-                    caminho = download_audio(links[-1], filenames[-1])
-                    
-                    with open(caminho, "rb") as audio:
-                        bot.send_audio(message.chat.id, audio, caption=f"<b>{titulo}</b>\n\n<b>Criador: </b><a href='https://github.com/paulowelton'>GitHub</a>", parse_mode='HTML')
-                    
-                    bot.delete_message(message.chat.id, message.message_id)
-                    
-                    print('audio enviado\n')
-                    
-                    apagar_arquivos(caminho)
-                                
-                                
-                                
-                                
-                                
-                if call.data == "option_cancel":
-                    log.info('usuario cancelou o download')
-                    
-                    bot.delete_message(message.chat.id, message.message_id)
-                    bot.send_message(message.chat.id, 'Canceled')
-
         except Exception as e:
             log.error(e)
             bot.send_message(message.chat.id, "O Download falhou, tente novamente...")
