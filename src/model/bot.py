11c5356167679f80131b2ef65f725e8fc82c5ca8
Paulo Welton <paulinho.welton08@gmail.com>
2025-04-15 11:17:36 -0300

    colocando variaveis de ambiente

--- src/model/bot.py
+++ src/model/bot.py
@@ -1,11 +1,13 @@
 import logging as log
+import os
 import time
 import telebot
+from dotenv import load_dotenv
 from src.model.handlers.command_handler import command_handlers
 from src.model.handlers.link_youtube_handler import link_youtube_handler
 from src.model.handlers.callback_handler import callback_handler
 
-BOT_TOKEN = '8008948209:AAGcIJmEhB81t-oKNJsljyG_4brDM5y8Ifk'
+load_dotenv()
 
 YOUTUBE_REGEX = r"(https?://)?(www\.)?(youtube\.com|youtu\.be)/\S+"
 
@@ -13,7 +15,7 @@ def bot():
     print('bot chamado')
     log.info('bot chamado')
     
-    bot = telebot.TeleBot(BOT_TOKEN)
+    bot = telebot.TeleBot(os.getenv("TOKEN_BOT"))
     
     bot.remove_webhook()
     time.sleep(1)
